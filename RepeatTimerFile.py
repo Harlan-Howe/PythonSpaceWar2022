a3016255054437a09b9b2e5f2ce9d962a8b2bbd1
Harlan Howe <hhowe7@gatech.edu>
2022-10-16 23:23:28 -0500

    Added credit where due!

--- RepeatTimerFile.py
+++ RepeatTimerFile.py
@@ -1,5 +1,11 @@
 from threading import Timer
 
+"""
+Found at https://stackoverflow.com/questions/12435211/threading-timer-repeat-function-every-n-seconds
+
+Code by right2clicky, based on Hans Then's original example
+"""
+
 class RepeatTimer(Timer):
     def run(self):
         while not self.finished.wait(self.interval):
