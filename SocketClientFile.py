7227ec7258ad14efd24ea63a6cfa08032181e9be
Harlan Howe <hhowe7@gatech.edu>
2023-10-17 10:11:28 -0500

    Style fixes

--- SocketClientFile.py
+++ SocketClientFile.py
@@ -49,8 +49,8 @@ class SocketClient:
         while self.keep_listening:
             try:
                 message_type, message = self.manager.receive_message_from_socket(connection)
-            except (ConnectionAbortedError, OSError) as Err: # if the socket is dropped, bail out.
-                # print(Err)
+            except (ConnectionAbortedError, OSError) as Err:  # if the socket is dropped, bail out.
+                print(Err)
                 self.keep_listening = False
                 break
             # print(f"{message_type=}\t{message=}")
@@ -74,19 +74,12 @@ class SocketClient:
             user_id = int(values[1])
             self.client_gui.delete_item_from_world(values[0], user_id)
 
-
-
-
-
-
-
     def handle_world_update(self, tab_delimited_world_list_string: str) -> None:
         self.world_contents = []
         lines = tab_delimited_world_list_string.split("\n")
         for line in lines:
             values = line.split("\t")
-            game_object = {}
-            game_object["type"] = values[0]
+            game_object = {"type": values[0]}
             if values[0] == "":
                 continue
             if values[0] == "PLAYER":
@@ -111,7 +104,7 @@ class SocketClient:
             self.world_contents.append(game_object)
         self.client_gui.update_world(self.world_contents)
 
-    def handle_user_list_update(self, tab_delimited_user_list_string:str) -> None:
+    def handle_user_list_update(self, tab_delimited_user_list_string: str) -> None:
         """
         The host has sent a tab-delimited string describing an updated user list; update the user_list in memory and
         onscreen.
@@ -120,13 +113,12 @@ class SocketClient:
         """
         self.update_user_list(tab_delimited_user_list_string)
         print("------------------")
-        for i in range(len(user_list)):
-            print(f"{i}\t{user_list[i]}")
+        for i in range(len(self.user_list)):
+            print(f"{i}\t{self.user_list[i]}")
         print("------------------")
-        self.client_gui.set_user_list(user_list)
+        self.client_gui.set_user_list(self.user_list)
 
-
-    def handle_receive_submission(self, submission:str) -> None:
+    def handle_receive_submission(self, submission: str) -> None:
         """
         The host has sent a string from one of the users (or the host, itself) that should be posted; do so!
         :param submission: the string to post
@@ -135,12 +127,12 @@ class SocketClient:
         print(f"MSG: {submission}")
         self.client_gui.add_to_chat(submission)
 
-
     def update_user_list(self, tab_delimited_user_list_string: str) -> None:
         """
         parse the tab-delimited string that contains the current list of users, replacing the information currently held
         in user_list.
-        :param tab_delimited_user_list_string: a tab-delimited string that holds a number of users and then the list of usernames
+        :param tab_delimited_user_list_string: a tab-delimited string that holds a number of users and then the list of
+        usernames
         :return: None
         """
 
@@ -148,12 +140,11 @@ class SocketClient:
 
         parts = tab_delimited_user_list_string.split("\t")
         print(f"{parts=}")
-        user_list.clear()
+        self.user_list.clear()
         num_users = int(parts[0])
         for i in range(1, num_users+1):
-            user_list.append(parts[i])
+            self.user_list.append(parts[i])
         self.client_gui.set_user_list(parts[1:0])
-
 
     def send_message(self, message: str) -> None:
         """
@@ -163,8 +154,7 @@ class SocketClient:
         """
         self.manager.send_message_to_socket(message, self.my_socket)
 
-
-    def close_socket(self, )  -> None:
+    def close_socket(self, ) -> None:
         """
         cease listening for the socket and shut down the socket.
         :return: None
@@ -180,4 +170,3 @@ class SocketClient:
 
 if __name__ == '__main__':
     client = SocketClient()
-
